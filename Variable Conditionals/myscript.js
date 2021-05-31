7356cc1eca5c16c08f523979e6ea78ab8e0ccaaf
G7-WHJR <raveen.anand@whitehatjr.com>
2021-05-31 12:22:12 +0530

    First

---
+++ Variable Conditionals/myscript.js
@@ -0,0 +1,22 @@
+function check(){
+
+    // 1. Take input from input box and store it in a variable named input
+
+
+
+    // 2. Calculate length of the input and store it in a variable name count.
+
+
+
+
+    //3. Check :
+        //if count is greater than 5 then 
+                //print a message "Entered string has more than 5 characters" 
+        //else 
+                //print "Entered string has less than 5 characters"
+
+
+                
+
+}
+
